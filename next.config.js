adfce3565c9e518c0fcea495f113820ff433dfe6
AdrienMoullec <s4209664@glos.ac.uk>
2025-05-09 17:09:48 +0100

    images unoptimized

--- next.config.js
+++ next.config.js
@@ -5,7 +5,8 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output:'export',
   assetPrefix: isProd ? '/NextJS-Portfolio' : '',
-  basePath: isProd ? '/NextJS-Portfolio' : ''
+  basePath: isProd ? '/NextJS-Portfolio' : '',
+  images: { unoptimized: true }
 };
 
 export default nextConfig;
