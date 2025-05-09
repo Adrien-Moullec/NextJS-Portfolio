87cf70e58db52a2ef1d17cb6ad4ddd34fe92ca1c
AdrienMoullec <s4209664@glos.ac.uk>
2025-05-09 16:49:39 +0100

    Testing basepath thing

--- components/LayoutChunks/ProjectCard.tsx
+++ components/LayoutChunks/ProjectCard.tsx
@@ -5,7 +5,7 @@ const ProjectCard = ({number, Title, Description} : {number:number, Title:string
         <div key={number}
         className="place-items-center border-1 flex flex-col p-5 mx-auto
         border-pink-400 hover:bg-amber-300 text-center">
-            <Image alt="project image" src="/Pictures/cat.png" width={150} height={150}
+            <Image alt="project image" src={"${process.env.NEXT_PUBLIC_BASE_PATH}/Pictures/cat.png"} width={150} height={150}
             className="border-1 border-black aspect-square rounded-2xl"
             />
             <div className="place-content-center-safe border-3 border-black">
