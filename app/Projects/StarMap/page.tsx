f8eb09f239529e0f73d695e02fd08b4382c82466
AdrienMoullec <amoullec1@gmail.com>
2025-09-14 23:25:09 +0100

    Better folder management

--- app/Projects/StarMap/page.tsx
+++ app/Projects/StarMap/page.tsx
@@ -18,19 +18,19 @@ export default function Home() {
     },[])
     return (
 
-        <div> 
+        <div className="flex flex-col items-center"> 
         <div id="PROJECT GRID" 
-        className="flex flex-row items-center justify-center pt-10 border-1 border-yellow-400 w-screen xl:border-black ">
+        className="flex flex-row items-center justify-center pt-10 border-1 border-yellow-400 w-screen xl:border-black">
             <Image className="ps-1" alt="project image" src={basePath+"/Pictures/cat.png"} width={150} height={150}/>
             <Image className="ps-1" alt="project image" src={basePath+"/Pictures/cat.png"} width={150} height={150}/>
             <Image className="ps-1" alt="project image" src={basePath+"/Pictures/cat.png"} width={150} height={150}/>
         </div>
-        <div className="w-[screen] h-170 border-yellow-400 invisible xl:visible">
+        <div className="w-250 h-170 border-yellow-400 border-4 invisible xl:visible">
             <iframe
             src="/index.html"
             width="100%"
             height="100%"
-            style={{border:"none", position:"relative", top:0, left:0,right:0,bottom:0}}
+            style={{border:"border-yellow-400 border:5", position:"relative", top:0, left:0,right:0,bottom:0}}
             allowFullScreen
             />
         </div><div id="PROJECT GRID" 
