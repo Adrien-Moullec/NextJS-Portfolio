be70c362a54963173f4ded03830ce366d4c862a9
AdrienMoullec <amoullec1@gmail.com>
2026-05-28 19:20:39 +0100

    webp slight updates

--- app/header.tsx
+++ app/header.tsx
@@ -19,7 +19,7 @@ export const Header = () => {
                 md:min-w-[500px] md:max-w-[1000px] pl-10">
                 <div id="Main ID" className="flex flex-col place-items-center pr-8">
                     <Avatar className="w-23 h-23">
-                        <AvatarImage src={basePath + "/Icons/images/definitely_me.png"} />
+                        <AvatarImage src={basePath + "/Icons/images/definitely_me.webp"} />
                         <AvatarFallback>CN</AvatarFallback>
                     </Avatar>
                     <p>{data.devInfo.devName}</p>
@@ -28,7 +28,7 @@ export const Header = () => {
                 <div id="Contact info" className="flex flex-col text-sm pt-3s">
                     <button onClick={() => CopyText(data.devInfo.devEmail)} className="flex flex-row place-items-center pb-2">
                         <Image
-                            alt="mail icon" src={basePath + "/Icons/email-icon.png"} width={50} height={50}
+                            alt="mail icon" src={basePath + "/Icons/email-icon.webp"} width={50} height={50}
                             className="pr-3 invert"
                         />
                         {data.devInfo.devEmail}
@@ -37,13 +37,13 @@ export const Header = () => {
                     <Link href={data.devInfo.devLinkedIn} className="flex flex-row place-items-center pb-2" target="_blank">
                         <Image
                             className="pr-3 invert"
-                            alt="LinkedIn Icon" src={basePath + "/Icons/linkedin-icon.png"} width={50} height={50}
+                            alt="LinkedIn Icon" src={basePath + "/Icons/linkedin-icon.webp"} width={50} height={50}
                         />
                         LinkedIn
                     </Link>
                     <Link href={data.devInfo.devGithub} className="flex flex-row place-items-center pb-2" target="_blank">
                         <Image
-                            alt="Github Icon" src={basePath + "/Icons/github-logo.png"} width={50} height={50}
+                            alt="Github Icon" src={basePath + "/Icons/github-logo.webp"} width={50} height={50}
                             className="pr-3 invert"
                         />
                         GitHub
